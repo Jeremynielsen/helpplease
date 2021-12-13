181013b0e6b14429d51a43b41f388dd213b9b3e4
Jeremynielsen <95871403+Jeremynielsen@users.noreply.github.com>
2021-12-13 10:08:23 -0500

    Create scrabblechallenge.js

---
+++ scrabblechallenge.js
@@ -0,0 +1,9 @@
+const scoreScrabble = function(str) {
+    const sepLetters = scoreScrabble.split(' ');
+    console.log(sepLetters);
+    
+
+    
+
+// your code here - don't forget to return a number!
+};
